e7c2e8db3ae91f48deaae6ea123ada966ba2f1d2
Crazy001Scientist <119999701+Crazy001Scientist@users.noreply.github.com>
2022-12-22 18:37:50 +0500

    Add files via upload

---
+++ covidData.sql
@@ -0,0 +1,50 @@
+
+select *
+from portfolioProgect..covid
+order by location
+
+--deth percantage
+select location, date, population, total_cases, total_deaths,
+(total_deaths/total_cases)*100 as DeathPercentage, 
+(total_cases/population)*100 as DeathPercentageToThePopulation
+from portfolioProgect..covid
+order by 1,2 
+ 
+--total death count in the continents
+ select continent, 
+Max(convert(int, total_deaths)) as TotalDeathCount
+from portfolioProgect..covid
+where continent is not NULL
+group by continent
+order by TotalDeathCount desc
+
+--total death in a location
+ select continent, location, 
+Max(convert(int, total_deaths)) as TotalDeathCount
+from portfolioProgect..covid
+where continent is not null
+group by continent, location
+order by TotalDeathCount desc
+
+--total vaccination count
+select continent, location, date, convert(bigint, new_vaccinations) as Vaccination,
+sum(convert(bigint, new_vaccinations)) 
+over (partition by location order by location, date)
+as TotalVaccinetionCount
+from portfolioProgect..covid
+where continent is not null
+order by location, date
+
+--percentage of population vaccinated
+select continent, location, date, new_cases,
+sum(new_cases) over (partition by location order by location, date)
+as TotalCasesCount,
+convert(bigint, new_vaccinations) as NewVaccination,
+sum(convert(bigint, new_vaccinations)) 
+over (partition by location order by location, date)
+as TotalVaccinetionCount,
+(people_vaccinated/nullif(total_cases, 0))*100 as PersentageOfVacciantedInfectedPeople,
+(people_vaccinated/population)*100 as PersentageOfPopulationVaccianted
+from portfolioProgect..covid
+where continent is not null
+order by location, date
